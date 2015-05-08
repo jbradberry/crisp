5cbfa86f1a4dac6d9425811d539917aed2b0b47e
Jeff Bradberry <jeff.bradberry@gmail.com>
2015-05-08 11:20:52 -0400

    Removed the recursion for deeper hailstone calculations.

--- crisp/interpreter.py
+++ crisp/interpreter.py
@@ -1,4 +1,15 @@
 
 
+def hailstone(number):
+    if number & 1:
+        return 3 * number + 1
+    else:
+        return number / 2
+
+
 def interpret(program):
-    print program
+    value = int(program.split('\n')[0])
+    print value
+    while value != 1:
+        value = hailstone(value)
+        print value
